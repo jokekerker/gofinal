13d0fadfd2bbbf4739b0ac170cddae2619e81ff8
papatpon.d <papatpon.d@kbtg.tech>
2020-11-21 13:57:59 +0700

    add model

--- customer/customer.go
+++ customer/customer.go
@@ -1,7 +1,7 @@
 package gofinal
 
 type Customer struct {
-	Id     int    `json:"id"`
+	ID     int    `json:"id"`
 	Name   string `json:"title"`
 	Email  string `json:"email"`
 	Status string `json:"status"`
